e093b0fd1fc0c4c3225b63b2eb7a29fab3ebfefc
netaction <schmidt@netaction.de>
2013-08-04 17:34:02 +0200

    Design

---
+++ routes/likeImage.js
@@ -0,0 +1,29 @@
+var
+	request = require('request'),
+	url = require('url'),
+	login = require('./login');
+
+
+module.exports = function(req, res){
+	var logindata = login(req,res);
+	if (!logindata) return;
+
+	var j = request.jar();
+	j.add(request.cookie('mk4_userid='+logindata.userid));
+	j.add(request.cookie('mk4_userpw='+logindata.userpw));
+
+	var queryData = url.parse(req.url,true).query;
+	request(
+		{
+			method:'POST',
+			url:'https://www.model-kartei.de/js/p/flpic.php',
+			form:{pid:queryData.imageNumber,typus:'love',ds9:queryData.ds9},
+			jar: j
+		},
+		function (error, response, body) {
+			res.writeHead(200, {'Content-Type': 'text/plain'});
+			res.write(JSON.stringify({ok:true}));
+			res.end();
+		}
+	);
+}
