8791acc8c07b6c8ffc6d54d424115384af2d3316
netaction <schmidt@netaction.de>
2013-08-05 16:22:18 +0200

    Design

--- routes/jobs.js
+++ routes/jobs.js
@@ -20,24 +20,38 @@ function getJobs(logindata,pageUrl,callback) {
 		function (error, response, body) {
 			try {
 			var jobs = body.split('class="bg');
+			/* 2">
+<div class="pleft40"><a class="jobtfp40" href="https://www.model-kartei.de/jobs/269451-0-suche-fotograf-fr-shooting-mit-2-lteren-damen.html" title="Suche Fotograf f&uuml;r Shooting mit 2 &auml;lteren Damen"></a></div>
+<div class="marl50">
+<div class="mrgnb4"><a class="bl ttb fs12" href="https://www.model-kartei.de/jobs/269451-0-suche-fotograf-fr-shooting-mit-2-lteren-damen.html" title="Suche Fotograf f&uuml;r Shooting mit 2 &auml;lteren Damen">Suche Fotograf f&uuml;r Shooting mit 2 &auml;lteren Damen</a><div class="fright c888 fs11">11.08.2013 - 15.08.2013</div></div>
+<div class="mrgnb8 c888"><code class="scd1" title="Model weiblich"></code> <a class="gr fs12" href="https://www.model-kartei.de/sedcards/model/289192/romina/" title="Romina...">Romina...</a></div>
+<div class="c888 fs12"><code class="flag fla1"></code> 13353 Berlin<div class="fright c888 fs12">TfP f&uuml;r <code class="scd2 ml2" title="Fotograf"></code></div><div class="cl"></div></div></div><div class="cl"></div></li>
+<li class=" */
 			jobs.splice(0,1); // remove first element
 			var view = fetchGlobalInfos(body);
 			view.jobs = [];
 			jobs.forEach(function(job){
 				var jobNumber = job.split('href="https://www.model-kartei.de/jobs/');
+				if (jobNumber.length==1) throw 'jobNumber fucked';
 				jobNumber = jobNumber[1].split('-0-');
 				jobNumber = jobNumber[0];
 
 				var jobTitle = job.split('title="');
+				if (jobTitle.length==1) throw 'jobTitle fucked';
 				jobTitle = jobTitle[1].split('">');
 				jobTitle = jobTitle[0];
 
 				var userSedcard = job.split('https://www.model-kartei.de/sedcards/');
+				if (userSedcard.length==1) { // User does not exist
+					return;
+				}
 				userSedcard = userSedcard[1].split('/" ');
 				userSedcard = userSedcard[0];
 
 				var userName = job.split('https://www.model-kartei.de/sedcards');
+				if (userName.length==1) throw 'userName 1 fucked';
 				userName = userName[1].split('title="');
+				if (userName.length==1) throw 'userName 2 fucked';
 				userName = userName[1].split('">');
 				userName = userName[0];
 
@@ -96,7 +110,6 @@ function displaySite(error,res,data) {
 		res.writeHead(404, {'Content-Type': 'text/plain'});
 		res.write('404 Not Found. Error in jobs.js\n');
 		res.end();
-		console.log(data);
 	}
 } // displaySite
 
